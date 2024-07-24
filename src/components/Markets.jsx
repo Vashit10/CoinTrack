5dda745808fe96b453c6aacfea65b29373d2290f
Vashit10 <118956102+Vashit10@users.noreply.github.com>
2024-07-24 17:14:22 +0530

    Update Markets.jsx

--- src/components/Markets.jsx
+++ src/components/Markets.jsx
@@ -3,7 +3,7 @@ import Coin from "./Coin";
 import Skeleton from "./Skeleton";
 
 const Markets = () => {
-  const { response, loading } = useAxios('coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+  const { response, loading } = useAxios('coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false');
 
   if(loading) {
     return (
