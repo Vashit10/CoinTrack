184d16b67ae0a5dc36b50ee7a1db7b80c4b5a507
Vashit10 <118956102+Vashit10@users.noreply.github.com>
2024-07-24 17:04:10 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className="flex items-center gap-1 cursor-pointer" onClick={() => navigate('/')}>
           <LogoIcon />
           <p className="font-semibold">
-            <span className="text-yellow-500">C</span>ypto<span className="text-yellow-500">U</span>pdate
+            <span className="text-yellow-500">C</span>rypto<span className="text-yellow-500">U</span>pdate
           </p>
         </div>
       </div>
